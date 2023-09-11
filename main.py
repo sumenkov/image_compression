ca1a0e2dd2ee396b93d0b8b21b78876ea9b20864
sdv <34712723+sumenkov@users.noreply.github.com>
2023-09-11 19:43:41 +0700

    Update main.py
    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,15 @@ def compress_img(image_name, new_size_ratio=1.0, quality=90, width=None, height=
     # загрузить изображение в память
     img = Image.open(image_name)
     print("[*] File name:", image_name.split("/")[1])
+    
     # распечатать исходную форму изображения
     print("[*] Image shape:", img.size)
+    
     # получить исходный размер изображения в байтах
     image_size = os.path.getsize(image_name)
     # распечатать размер перед сжатием/изменением размера
     print("[*] Size before compression:", get_size_format(image_size))
+    
     if new_size_ratio < 1.0:
         # если коэффициент изменения размера ниже 1,0, умножьте ширину и высоту на этот коэффициент, чтобы уменьшить размер изображения.
         img = img.resize((int(img.size[0] * new_size_ratio), int(img.size[1] * new_size_ratio)), Image.ANTIALIAS)
@@ -43,10 +46,12 @@ def compress_img(image_name, new_size_ratio=1.0, quality=90, width=None, height=
         img = img.resize((width, height), Image.ANTIALIAS)
         # распечатать новую форму изображения
         print("[+] New Image shape:", img.size)
+        
     # разделить имя файла и расширение
     filename, ext = os.path.splitext(image_name.split("/")[1])
     # сделать новое имя файла, добавив _compressed к исходному имени файла
     directory = os.path.dirname("./new_image/")
+    
     if not os.path.exists(directory):
         os.makedirs(directory)
 
@@ -57,18 +62,21 @@ def compress_img(image_name, new_size_ratio=1.0, quality=90, width=None, height=
         # сохранить то же расширение исходного изображения
         new_filename = f"{directory}/{filename}{ext}"
     try:
-        # сохраните изображение с соответствующим качеством и установите для оптимизации значение True
+        # сохраняем изображение с соответствующим качеством и установите для оптимизации значение True
         img.save(new_filename, quality=quality, optimize=True, progressive=True)
     except OSError:
         # сначала преобразовать изображение в режим RGB
         img = img.convert("RGB")
-        # сохраните изображение с соответствующим качеством и установите для оптимизации значение True
+        # повторим сохранение
         img.save(new_filename, quality=quality, optimize=True, progressive=True)
+        
     print("[+] New file saved:", new_filename)
+    
     # получить новый размер изображения в байтах
     new_image_size = os.path.getsize(new_filename)
     # распечатать новый размер в хорошем формате
     print("[+] Size after compression:", get_size_format(new_image_size))
+    
     if new_image_size > image_size:
         os.remove(new_filename)
         print("[+] New file remove:", new_filename)
@@ -77,6 +85,7 @@ def compress_img(image_name, new_size_ratio=1.0, quality=90, width=None, height=
         saving_diff = new_image_size - image_size
         # распечатать процент экономии
         print(f"[+] Image size change: {saving_diff/image_size*100:.2f}% of the original image size.")
+        
     print("-------------------------------------------------------------------------------------\n")
 
 
